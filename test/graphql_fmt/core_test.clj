2cabcdad9575c622994c1d9dbd1d64aa8a0380eb
Ilmo Raunio <ilmo.raunio@metosin.fi>
2019-11-15 00:01:59 +0200

    Add FloatValue to ebnf

--- test/graphql_fmt/core_test.clj
+++ test/graphql_fmt/core_test.clj
@@ -19,4 +19,116 @@
          (graphql-parser "10")))
   (is (= [:Token
           [:IntValue [:IntegerPart [:NegativeSign "-"] [:NonZeroDigit "1"] [:Digit "0"]]]]
-         (graphql-parser "-10"))))
+         (graphql-parser "-10")))
+  (is (= [:Token
+          [:FloatValue
+           [:IntegerPart [:NonZeroDigit "1"]]
+           [:FractionalPart "." [:Digit "0"]]]]
+         (graphql-parser "1.0")))
+  (is (= [:Token
+          [:FloatValue
+           [:IntegerPart [:NegativeSign "-"] [:NonZeroDigit "1"]]
+           [:FractionalPart "." [:Digit "0"]]]]
+         (graphql-parser "-1.0")))
+  (is (= [:Token [:FloatValue [:IntegerPart "0"] [:FractionalPart "." [:Digit "1"]]]]
+         (graphql-parser "0.1")))
+  (is (= [:Token
+          [:FloatValue
+           [:IntegerPart [:NegativeSign "-"] "0"]
+           [:FractionalPart "." [:Digit "1"]]]]
+         (graphql-parser "-0.1")))
+  (is (= [:Token [:FloatValue [:IntegerPart "0"] [:FractionalPart "." [:Digit "0"]]]]
+         (graphql-parser "0.0")))
+  (is (= [:Token
+          [:FloatValue
+           [:IntegerPart [:NegativeSign "-"] "0"]
+           [:FractionalPart "." [:Digit "0"]]]]
+         (graphql-parser "-0.0")))
+  (is (= [:Token
+          [:FloatValue
+           [:IntegerPart [:NonZeroDigit "1"]]
+           [:ExponentPart [:ExponentIndicator "e"] [:Digit "5"] [:Digit "0"]]]]
+         (graphql-parser "1e50")))
+  (is (= [:Token
+          [:FloatValue
+           [:IntegerPart "0"]
+           [:ExponentPart [:ExponentIndicator "e"] [:Digit "5"] [:Digit "0"]]]]
+         (graphql-parser "0e50")))
+  (is (= [:Token
+          [:FloatValue
+           [:IntegerPart "0"]
+           [:ExponentPart [:ExponentIndicator "e"] [:Digit "0"]]]]
+         (graphql-parser "0e0")))
+  (is (= [:Token
+          [:FloatValue
+           [:IntegerPart [:NegativeSign "-"] "0"]
+           [:ExponentPart [:ExponentIndicator "e"] [:Digit "0"]]]]
+         (graphql-parser "-0e0")))
+  (is (= [:Token
+          [:FloatValue
+           [:IntegerPart [:NegativeSign "-"] "0"]
+           [:ExponentPart [:ExponentIndicator "e"] [:Digit "5"] [:Digit "0"]]]]
+         (graphql-parser "-0e50")))
+  (is (= [:Token
+          [:FloatValue
+           [:IntegerPart [:NegativeSign "-"] [:NonZeroDigit "1"]]
+           [:ExponentPart [:ExponentIndicator "e"] [:Digit "5"] [:Digit "0"]]]]
+         (graphql-parser "-1e50")))
+  (is (= [:Token
+          [:FloatValue
+           [:IntegerPart "0"]
+           [:ExponentPart [:ExponentIndicator "e"] [:Sign "-"] [:Digit "0"]]]]
+         (graphql-parser "0e-0")))
+  (is (= [:Token
+          [:FloatValue
+           [:IntegerPart "0"]
+           [:ExponentPart [:ExponentIndicator "e"] [:Sign "-"] [:Digit "5"] [:Digit "0"]]]]
+         (graphql-parser "0e-50")))
+  (is (= [:Token
+          [:FloatValue
+           [:IntegerPart [:NonZeroDigit "1"]]
+           [:ExponentPart [:ExponentIndicator "e"] [:Sign "-"] [:Digit "5"] [:Digit "0"]]]]
+         (graphql-parser "1e-50")))
+  (is (= [:Token
+          [:FloatValue
+           [:IntegerPart [:NegativeSign "-"] "0"]
+           [:ExponentPart [:ExponentIndicator "e"] [:Sign "-"] [:Digit "0"]]]]
+         (graphql-parser "-0e-0")))
+  (is (= [:Token
+          [:FloatValue
+           [:IntegerPart [:NegativeSign "-"] "0"]
+           [:ExponentPart [:ExponentIndicator "e"] [:Sign "-"] [:Digit "5"] [:Digit "0"]]]]
+         (graphql-parser "-0e-50")))
+  (is (= [:Token
+          [:FloatValue
+           [:IntegerPart [:NegativeSign "-"] [:NonZeroDigit "1"]]
+           [:ExponentPart [:ExponentIndicator "e"] [:Sign "-"] [:Digit "5"] [:Digit "0"]]]]
+         (graphql-parser "-1e-50")))
+  (is (= [:Token
+          [:FloatValue
+           [:IntegerPart [:NonZeroDigit "6"]]
+           [:FractionalPart
+            "."
+            [:Digit "0"]
+            [:Digit "2"]
+            [:Digit "2"]
+            [:Digit "1"]
+            [:Digit "4"]
+            [:Digit "1"]
+            [:Digit "3"]]
+           [:ExponentPart [:ExponentIndicator "e"] [:Digit "2"] [:Digit "3"]]]]
+         (graphql-parser "6.0221413e23")))
+  (is (= [:Token
+          [:FloatValue
+           [:IntegerPart [:NegativeSign "-"] [:NonZeroDigit "6"]]
+           [:FractionalPart
+            "."
+            [:Digit "0"]
+            [:Digit "2"]
+            [:Digit "2"]
+            [:Digit "1"]
+            [:Digit "4"]
+            [:Digit "1"]
+            [:Digit "3"]]
+           [:ExponentPart [:ExponentIndicator "e"] [:Sign "-"] [:Digit "2"] [:Digit "3"]]]]
+         (graphql-parser "-6.0221413e-23"))))
