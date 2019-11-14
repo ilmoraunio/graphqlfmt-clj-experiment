29ec97864fcf66dd0b2eb47f50a6ad79a844f77f
Ilmo Raunio <ilmo.raunio@metosin.fi>
2019-11-14 23:32:55 +0200

    Add IntValue to ebnf

--- test/graphql_fmt/core_test.clj
+++ test/graphql_fmt/core_test.clj
@@ -1,9 +1,22 @@
 (ns graphql-fmt.core-test
   (:require [clojure.test :refer [are deftest is testing]]
-            [graphql-fmt.core :as core]))
+            [graphql-fmt.core :refer [graphql-parser]]))
 
 (deftest test-tokens
-  (is (= [:Punctuator "{"]
-         (core/graphql-parser "{")))
+  (is (= [:Token [:Punctuator "{"]]
+         (graphql-parser "{")))
   (is (= [:Token [:Name "frob"]]
-         (core/graphql-parser "frob"))))
+         (graphql-parser "frob")))
+  (is (= [:Token [:IntValue [:IntegerPart "0"]]]
+         (graphql-parser "0")))
+  (is (= [:Token [:IntValue [:IntegerPart [:NegativeSign "-"] "0"]]]
+         (graphql-parser "-0")))
+  (is (= [:Token [:IntValue [:IntegerPart [:NonZeroDigit "1"]]]]
+         (graphql-parser "1")))
+  (is (= [:Token [:IntValue [:IntegerPart [:NegativeSign "-"] [:NonZeroDigit "1"]]]]
+         (graphql-parser "-1")))
+  (is (= [:Token [:IntValue [:IntegerPart [:NonZeroDigit "1"] [:Digit "0"]]]]
+         (graphql-parser "10")))
+  (is (= [:Token
+          [:IntValue [:IntegerPart [:NegativeSign "-"] [:NonZeroDigit "1"] [:Digit "0"]]]]
+         (graphql-parser "-10"))))
