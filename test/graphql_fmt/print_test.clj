2e669523cd6cbfc7d0d2c9d6af74b4e2c762e27f
Ilmo Raunio <ilmo.raunio@gmail.com>
2020-09-26 15:25:27 +0300

    Add passing test for non-null NamedType within VariableDefinitions list
    Also, rename VariableDefinitions test filenames.

--- test/graphql_fmt/print_test.clj
+++ test/graphql_fmt/print_test.clj
@@ -22,7 +22,6 @@
               (insta/transform
                 transform-map
                 (document-parser input)))))
-    "query frob($foo:[bar!]) {a,b}"
     "query frob($foo:[bar!]!) {a,b}"
     "query frob($foo:bar=true) {a,b}"
     "fragment foo on Bar @foobar {a,b,c}"
